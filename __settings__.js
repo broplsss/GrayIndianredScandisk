1f55ce6694b7e05c03550e3dffcc3baa0fa7f88e
broplsss <112293999+broplsss@users.noreply.github.com>
2023-06-29 14:27:14 +0000

    Initial commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1111800.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 44330128, 44330076, 44305490, 44305491, 44305492, 44305493, 44305494, 44305497, 44305498, 44305499, 44305501, 44305502, 44305500, 44305514, 44305516, 44305517, 44305520, 44305521, 44305523, 44305525, 44305528, 44305534, 44305566, 44305567, 44305578, 44305579, 44305580, 44305582, 44305591, 44305594, 44305595, 44305599, 44305600, 44305601, 44305602, 44305603, 44305657, 44305658, 44305659, 44305660, 44305672, 44305680, 44305681, 44305685, 44305686, 44305701, 44305702, 44305703, 44305726, 44305728, 44305732, 44346960 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/44305454/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/44305455/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/44305453/1/ammo.js', 'preload' : true},
+];
